480be042946abfd3f280244605aff6da9b50be35
thesushantvaid <132909409+thesushantvaid@users.noreply.github.com>
2023-05-15 09:59:09 -0400

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,45 @@
+//task1
+
+let a = 3;
+let b = 5;
+let c;
+
+// alert( "description of alerted_value: " + alerted_value );
+
+let output = 
+`
+let a = 3;
+let b = 5;
+let c;
+------------
+a + b =${a + b}
+a - b =${a - b}
+a * b =${a * b}
+a / b =${a / b}
+a % b =${a % b}
+a += b =${a += b}
+`
+alert(output);
+
+/*
+a + b
+a - b
+a * b
+a / b
+a % b
+a += b
+a -= b
+a *= b
+a /= b
+a %= b
+a == b
+a != b
+a > b
+a < b
+!a && !c 
+!a || !c 
+*/
+
+//task2
+
+
